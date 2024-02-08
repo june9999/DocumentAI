da5f321ef6792179b90a68654b86e1c669020fbd
june9999 <vesper0@163.com>
2024-02-08 11:22:37 +0100

    create frontend structure

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
